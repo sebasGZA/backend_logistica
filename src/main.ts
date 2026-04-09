52c83b69b9938cb64a5a8dc5882b67c9a985a8e4
sebasGZA <sebassga34@gmail.com>
2026-04-09 09:34:35 -0500

    feat: configurar swagger

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 
 import { AppModule } from './app.module';
+import { swaggerConfiguration } from './swagger/config/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = parseInt(process.env.PORT ?? '3000');
+  const prefix = process.env.API_PREFIX ?? 'api'
+
+  swaggerConfiguration(app);
+  app.setGlobalPrefix(prefix);
 
   await app.listen(port, () => {
     Logger.log(`Server running on port ${port}`, 'main');
